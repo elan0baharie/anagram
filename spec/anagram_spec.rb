5858f39011536464b5d8b360469b88db5c4cc00f
Elan Baharie <0elan1@gmail.com>
2017-04-21 11:55:06 -0700

    pass second spec to account for case

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,6 +3,11 @@ require('anagram')
 
 describe('String#anagram') do
   it("will determine if the same letters are present in an example word") do
+    expect(("dear").anagram_mthd("read")).to(eq("These words are anagrams."))
+  end
+
+  it("will determine if the same letters are present in an example word regardless of case") do
     expect(("dear").anagram_mthd("READ")).to(eq("These words are anagrams."))
   end
+
 end
